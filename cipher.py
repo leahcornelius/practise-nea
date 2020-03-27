c083acedda0552b14b0a2d12e2ea36cf286a8220
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-03-27 08:57:27 +0000

    Fix some python specifics

--- cipher.py
+++ cipher.py
@@ -1,16 +1,16 @@
-// Copyright 2020 Leo Cornelius
-// This file has the functions to encode plaintext with a n steap setting or decrypt cifertextb with that setting
+# Copyright 2020 Leo Cornelius
+# This file has the functions to encode plaintext with a n steap setting or decrypt cifertextb with that setting
 
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-function encrypt(plaintext, step):
+def encrypt(plaintext, step):
     print("Encrypting plaintext: " + plaintext + ", using shift key: " + step)
     plaintext = plaintext.strip()
     for i in range(len(plaintext)):
          plantext[i] = chr(ord(plaintext[i]) + step)
     return plaintext
     
-function decrypt(ciphertext, step):
+def decrypt(ciphertext, step):
     print("Decrypting ciphertext: " + ciphertext + ", using shift key: " + step)
     ciphertext = ciphertext.strip()
     for i in range(len(ciphertext)):
